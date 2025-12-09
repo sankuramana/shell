4a6aa1a1cb0698eb12e6f4f24d996bef8e23ef6f
ramana sanku <sankuramana456@gmail.com>
2025-12-09 20:08:35 +0530

    shell-programms

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#conversation between two persions.
+echo "Ramesh:: Hi Suresh, How are you?"
+echo "Suresh:: Hello Ramesh, I am fine. How are you?"
+echo "Ramesh:: What are you learning recently?"
+echo "Suresh:: I am learning Shell scripting Ramesh"
