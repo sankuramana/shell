a9d616206a8e63f539b1d1bdf839f222ff5b1086
ramana sanku <sankuramana456@gmail.com>
2025-12-09 21:05:56 +0530

    shell-programms

---
+++ 06-variable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "please enter the pin number"
+read PIN_NUMBER #here PIN_NUMBER IS THE VARAIBLE 
+# syntax read variable_name
+# read is a built-in command in Bash that accepts input from the keyboard and stores it in a variable.
+echo "pin number is $PIN_NUMBER"
+
