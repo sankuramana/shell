8f12c31215c8d79cbcf2a5fbc54c1f91c87516f5
ramana sanku <sankuramana456@gmail.com>
2025-12-09 21:28:51 +0530

    shell-programms

--- 06-variable.sh
+++ 06-variable.sh
@@ -6,3 +6,8 @@ read -s PIN_NUMBER #hides the values
 # read is a built-in command in Bash that accepts input from the keyboard and stores it in a variable.
 echo "pin number is $PIN_NUMBER"
 
+
+
+# "reas -s"
+#t does NOT show the characters the user types.
+#Useful for passwords, secret keys, tokens, PIN, etc.
