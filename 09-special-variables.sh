4ec9b33696d5ffe174d2fba195c63793ec869f9f
ramana sanku <sankuramana456@gmail.com>
2025-12-10 12:50:10 +0530

    shell-practice

--- 09-special-variables.sh
+++ 09-special-variables.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-echo "print all varaible passed to the script : $@"
-echo "print all variable passsed to the script: $*"
-#each space will consider as one parameters
-echo "script name: $0"
-echo "current directory : $PWD"
-echo "current user name : $USER" #capital not small letter
-echo "home directory of the user: $HOME"
-echo "PID of the scripy: $$"
-sleep 15 & #this run backgroun not in forgrund
-echo "PID os the last command in background is : $!"
+
+# Print all variables passed to the script
+echo "All variables passed to the script (\$@): $@"
+echo "All variables passed to the script (\$*): $*"
+
+echo "Script name: $0"
+# Current directory
+echo "Current directory: $PWD"
+# Current user name
+echo "Current user name: $USER"   # Must be CAPITAL letters
+# User's home directory
+echo "Home directory of the user: $HOME"
+# PID of the current script
+echo "PID of the script: $$"
+# Run sleep in background (not in foreground)
+sleep 15 &
+# PID of the last command run in background
+echo "PID of the last background command: $!"
+echo " Exit status of last executed command: $?"
+
+#$1, $2, $3...	Positional parameters (values passed to the script)
