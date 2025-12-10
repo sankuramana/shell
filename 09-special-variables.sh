1721df897c508e0fd396ba2644841f76361d28cf
ramana sanku <sankuramana456@gmail.com>
2025-12-10 12:38:06 +0530

    shell-practice

--- 09-special-variables.sh
+++ 09-special-variables.sh
@@ -3,5 +3,7 @@ echo "print all varaible passed to the script : $@"
 echo "print all variable passsed to the script: $*"
 #each space will consider as one parameters
 echo "script name: $0"
-echo "current directory $PWD"
-echo "current user : $USER" #capital not small letter
+echo "current directory : $PWD"
+echo "current user name : $USER" #capital not small letter
+echo "home directory of the user: $HOME"
+
