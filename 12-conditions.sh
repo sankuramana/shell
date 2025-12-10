e8d48d864e142bba8c99979f5facfd798c7aa326
ramana sanku <sankuramana456@gmail.com>
2025-12-10 17:36:26 +0530

    shell-practice

--- 12-conditions.sh
+++ 12-conditions.sh
@@ -2,7 +2,7 @@
 #if number is divide by 2 the it is even else odd
 echo "please enter the number"
 read NUMBER
-if [ $(($NUMBER % 2 -eq 0)) ]; then
+if [ $(($NUMBER % 2 ))-eq 0 ]; then 
     echo "the entered number: $NUMBER is EVEN"
 else
     echo "The entered number : $NUMBER is ODD"
