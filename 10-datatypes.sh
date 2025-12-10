804272365b52bdfe1b0d7abbee8be2ddb966434d
ramana sanku <sankuramana456@gmail.com>
2025-12-10 14:43:40 +0530

    shell-practice

--- 10-datatypes.sh
+++ 10-datatypes.sh
@@ -4,7 +4,10 @@ NUMBER1=10
 NUMBER2=20
 NUMBER3="5"
 NUMBER4="1"
+NAME="DEVOPS"
 SUM=$(($NUMBER1+$NUMBER2))
 SUM2=$(($NUMBER3+$NUMBER4))
+SUM3=$(($NUMBER3+$NAME))
 echo "sum is : $SUM"
-echo "sum is : $SUM2"
+echo "sum2 is : $SUM2"
+echo "sum3 is : $SUM3"
