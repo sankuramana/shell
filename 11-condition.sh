ec740034e7f82e30c20f3035e91d23dd25c9bba9
ramana sanku <sankuramana456@gmail.com>
2025-12-10 16:35:39 +0530

    shell-practice

--- 11-condition.sh
+++ 11-condition.sh
@@ -2,9 +2,9 @@
 #checking if number is equal to 10 or lessthan or greaterthan
 NUMBER=$1
 
-if[ $NUMBER -lt 10 ]; then
+if [ $NUMBER -lt 10 ]; then
     echo "Given number is $NUMBER less than 10"
-elseif[ $NUMBER -eq 10  ]; then
+elseif [ $NUMBER -eq 10 ]; then
     echo "given number $NUMBER is equal to 10"
 else
     echo "given number is: $NUMBER greather than 10"
