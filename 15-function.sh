338d46a73c0099fff3b2f1b6d2beffd0d022ca9c
ramana sanku <sankuramana456@gmail.com>
2025-12-10 21:11:31 +0530

    shell-practice

--- 15-function.sh
+++ 15-function.sh
@@ -5,26 +5,21 @@ if [ $USERID -ne 0 ]; then
     exit 1
 fi
 
-dnf install mysql -y
-
-if [ $? -ne 0 ]; then
-    echo "mysql installation failed"
-    exit 1
-else
-    echo "mysql is installed successfully"
-fi
-#mysql installation is done
-#installing nginx and chceking its installation
-dnf install nginx -y
-if [ $? -ne 0 ]; then
-    echo "instalation of nginx is failed"
+VALIDATE()
+{
+if [ $1 -ne 0 ]; then
+    echo "instalation of $2 is failed"
     exit 1
 else 
-    echo "instalation of nginx is completed"
-fi 
-#same pyhton
+    echo "$2 is installed successfully"
+}
+
+dnf install mysql -y
+
+VALIDATE $? "MYSQL" #passing arguments while calling function and chceking status code of previous command
+dnf install nginx -y
+VALIDATE $? "NGINX"
+
 dnf install python3 -y
-if [ $? -ne 0 ]; then
-    echo "installtion of python is failed"
-    exit 1  
-fi
+VALIDATE $? "PYTHON3"
+
