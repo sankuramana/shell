763abd71f5b06da6f67769a7c1fc240a464e9946
ramana sanku <sankuramana456@gmail.com>
2025-12-10 12:40:14 +0530

    shell-practice

--- 09-special-variables.sh
+++ 09-special-variables.sh
@@ -6,4 +6,4 @@ echo "script name: $0"
 echo "current directory : $PWD"
 echo "current user name : $USER" #capital not small letter
 echo "home directory of the user: $HOME"
-
+echo "PID of the scripy: $$"
