c7e6ba4d70a1a54e8c0aaf54a7a93f39ea053c03
ramana sanku <sankuramana456@gmail.com>
2025-12-10 14:33:58 +0530

    shell-practice

--- 10-datatypes.sh
+++ 10-datatypes.sh
@@ -2,5 +2,9 @@
 #by default verything in shell "string"
 NUMBER1=10
 NUMBER2=20
+NUMBER3="5"
+NUMBER4="1"
 SUM=$(($NUMBER1+$NUMBER2))
+SUM2=$(($NUMBER3+$NUMBER4))
 echo "sum is : $SUM"
+echo "sum is : $SUM2"
