20344469523a3b62a8d68bbbdb133bb6ec6f092c
ramana sanku <sankuramana456@gmail.com>
2025-12-10 12:34:49 +0530

    shell-practice

--- 09-special-variables.sh
+++ 09-special-variables.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo "print all varaible passed to the script : $@"
 echo "print all variable passsed to the script: $*"
+#each space will consider as one parameters
+echo "script name: $0"
