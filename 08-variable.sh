d8c3d633f437e9e58c2f518e45a95414b09a7fdd
ramana sanku <sankuramana456@gmail.com>
2025-12-10 11:16:38 +0530

    shell-practice

--- 08-variable.sh
+++ 08-variable.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # inside shell script execute the coomand and stores the output
-START_TIME=$(date)
+START_TIME=$(date +%s) 
 sleep 10
 END_TIME=$(date +%s) #time in seconds
 TOTAL_TIME=$(($END_TIME-$START_TIME))
