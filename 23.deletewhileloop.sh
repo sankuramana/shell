f2db2be2fd9ae8e23637afcf5f804ab17fe19876
ramana sanku <sankuramana456@gmail.com>
2025-12-11 17:41:03 +0530

    docker-roboshop

--- 23.deletewhileloop.sh
+++ 23.deletewhileloop.sh
@@ -11,8 +11,9 @@ FILES_TO_DELETE=$(find $FOLDER_TO_CREATE -name "*.log" -mtime +14)
     
  fi
 while  IFS= read -r file
-do 
-echo "deleting file "
-echo " $file"
+    do 
+        echo "deleting file "
+        echo " $file"
+        rm -f $file
 
-done <<<"$FILES_TO_DELETE"
+    done <<<"$FILES_TO_DELETE"
