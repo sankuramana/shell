97214692af2adca7b1d6859b10021eb85729a17d
ramana sanku <sankuramana456@gmail.com>
2025-12-12 02:58:18 +0530

    docker-roboshop

---
+++ 28-backpM.sh
@@ -0,0 +1,17 @@
+USERID=$(id -u)
+#checking user or rootuser
+if[ $USERID -ne 0 ]; then
+    echo "please use root previliages for this"
+    exit 1
+ fi
+ #logs
+ LOGS_FOLDER=/var/log/shell-logs/
+ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+ LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME.log"
+  mkdir -p $LOGS_FOLDER
+
+ echo "folder creation is done $LOGS_FOLDER" &>>ShellOUTPUT.log
+ echo "SCRIPT_NAME iS $SCRIPT_NAME"
+ echo " $LOG_FILE"
+ 
+
