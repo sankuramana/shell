2970f1b93412f1c548d6371f9a316dc5f646a5c2
ramana sanku <sankuramana456@gmail.com>
2025-12-11 16:06:29 +0530

    docker-roboshop

--- 22-deletelogfiles.sh
+++ 22-deletelogfiles.sh
@@ -10,7 +10,7 @@ if [ ! -d $SOURCE_DIR ]; then
     echo "directory $SOURCE_DIR is not fount please create"
     exit 1
  fi
- FILES_TO_DELETE= $(find /var/log/app-logs/ -name "*.log" -mtime +14days)
+ FILES_TO_DELETE=$(find /var/log/app-logs/ -name "*.log" -mtime +14days)
  echo "Files to delete $FILES_TO_DELETE"
     
 
