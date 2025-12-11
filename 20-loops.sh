bc6a3dcd1fd44dae14ebf7999ecf870c9698734c
ramana sanku <sankuramana456@gmail.com>
2025-12-11 12:11:54 +0530

    docker-roboshop

--- 20-loops.sh
+++ 20-loops.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
-Y="\e[33m"
-N="\e[0m"
-echo "script executed at $(date)"
 
-for package in $@ #has to be next to eachother like no gap{1..10}
-do 
-dnf list installed $package
-if [ $? -ne 0 ];then
-dnf install $package
-else 
-    echo -e " $package is istalled already... $Y SKIPPING $N "
-fi
-done
+
+
+
+for i in {1..20}
+{
+    echo $i
+}
+##############################################################################
+# Y="\e[33m"
+# N="\e[0m"
+# echo "script executed at $(date)"
+
+# for package in $@ #has to be next to eachother like no gap{1..10}
+# do 
+# dnf list installed $package
+# if [ $? -ne 0 ];then
+# dnf install $package
+# else 
+#     echo -e " $package is istalled already... $Y SKIPPING $N "
+# fi
+# done
