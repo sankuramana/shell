8cdd93995a2124d35e178e1ca54408d8814ff967
ramana sanku <sankuramana456@gmail.com>
2025-12-11 07:04:59 +0530

    docker-roboshop

--- 17-colours.sh
+++ 17-colours.sh
@@ -4,9 +4,9 @@ GREEN="\e[32m"
 YELLOW="\e[33m"
 BLUE="\e[34m"
 NORMAL="\e[0m"
-echo -e "$RED printing red clor $N" #-e is mandatory
-echo -e "$GREEN printing green clor $N"
-echo -e "$YELLOW printing yellow clor $N"
-echo -e "$BLUE printing blue clor $N"
-echo -e "$NORMAL printing NORMAL/WHITE clor $N"
+echo -e "$RED printing red colour $N" #-e is mandatory
+echo -e "$GREEN printing green colour $N"
+echo -e "$YELLOW printing yellow colour $N"
+echo -e "$BLUE printing blue colour $N"
+echo -e "$NORMAL printing NORMAL/WHITE colour $N"
 
