e161bd56aa85e1fb2146419d9b0e5d5837e6d242
ramana sanku <sankuramana456@gmail.com>
2025-12-11 10:04:49 +0530

    docker-roboshop

--- 19-logs.sh
+++ 19-logs.sh
@@ -2,10 +2,46 @@
 R=”\e[31m”
 G=”\e[32m”
 Y=”\e[33m”
+N="\e[0m"
 
+LOGS_FOLDER="/var/log/installationlogs"
+SCRIT_NAME=$(echo $0 |cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRI_NAME.log"
+mkdir -p $LOGS_FOLDER
 if [ $USERID –ne 0 ]; then
 echo “use root user to create folder in varfolder”
 exit1
 fi
-mkdir –p /var/log/installationlogs
+VALIDATE()
+{
+if [ $1 -ne 0 ]; then
+    echo -e "  INSTALLING $2... $R is FAILURE $N"  &>>$LOG_FILE #-e is mandatory 
+    exit 1
+else 
+    echo -e "INSTALLTION $2.....  $G SUCCESS $N" &>>$LOG_FILE
+fi
+}
+dnf list installed mysql &>>$LOG_FILE
+if [ $? -ne 0 ];then
+dnf install mysql -y
+VALIDATE $? "MYSQL" #passing arguments while calling function &chceking status code of previous command
+else 
+    echo -e "MYSQL is already exsist $Y SKIPPING $N"
+fi
+dnf list installed nginx
+if [ $? -ne 0 ];then    
+dnf install nginx -y &>>$LOG_FILE
+VALIDATE $? "NGINX" &>>$LOG_FILE
+else 
+     echo -e "NGINX is Already exist $Y SKIPPING $N" 
+fi
+dnf list installed python3
+if [ $? -ne 0 ];then
+dnf install python3 -y
+VALIDATE $? "PYTHON3"
+else
+    echo -e "PYTHON3 EXIST already $Y SKIPPING $N"
+fi
+
+
 
