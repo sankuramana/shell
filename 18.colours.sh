08e4b84623271b855343139542e355ea4db95fb5
ramana sanku <sankuramana456@gmail.com>
2025-12-11 07:31:18 +0530

    docker-roboshop

--- 18.colours.sh
+++ 18.colours.sh
@@ -20,21 +20,21 @@ else
 fi
 }
 dnf list installed mysql
-if [ $? -ne 0];then
+if [ $? -ne 0 ];then
 dnf install mysql -y
 VALIDATE $? "MYSQL" #passing arguments while calling function &chceking status code of previous command
 else 
     echo -e "MYSQL is already exsist $y SKIPPING $N"
 fi
 dnf list installed nginx
-if [ $? -ne 0];then    
+if [ $? -ne 0 ];then    
 dnf install nginx -y
 VALIDATE $? "NGINX"
 else 
      echo -e "NGINX is Already exist $Y SKIPPING $N"
 fi
 dnf list installed python3
-if [ $? -ne 0];then
+if [ $? -ne 0 ];then
 dnf install python3 -y
 VALIDATE $? "PYTHON3"
 else
