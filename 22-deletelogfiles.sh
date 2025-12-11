4579d2e6a16b694e440495d5d20ebf652ec50b66
ramana sanku <sankuramana456@gmail.com>
2025-12-11 16:01:14 +0530

    docker-roboshop

--- 22-deletelogfiles.sh
+++ 22-deletelogfiles.sh
@@ -4,15 +4,13 @@ N="\e[0m"
 #app-logs Dircetory
 # file -name logfiles
 # -mtime +14days
+SOURCE_DIR=/var/log/app-logs
 mkdir -p $SOURCE_DIR
-SOURCE_DIR=/var/log
 if [ ! -d $SOURCE_DIR ]; then
     echo "directory $SOURCE_DIR is not fount please create"
-else
-    FILES_TO_DELETE=$(find -name "*.log" -mtime +14)
-    echo "existing files are $FILES_TO_DELETE"
-    rm -f $FILES_TO_DELETE
-    echo "deleted files $FILES_TO_DELETE"
-fi
+    exit 1
+ fi
+ FILES_TO_DELETE= $(find /var/log/app-logs/ -name "*.log" -mtime +14days)
+ echo "Files to delete $FILES_TO_DELETE"
     
 
