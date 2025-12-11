c1cfca873469c927ba69be81fbe88a473e80d121
ramana sanku <sankuramana456@gmail.com>
2025-12-11 12:03:29 +0530

    docker-roboshop

--- 20-loops.sh
+++ 20-loops.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+USERID=$(id -u)
+Y="e\[33m"
+if [$UERID -ne 0 ];then
+    echo " please run with root user"
+    exit 1
+fi
+echo "script executed at $(date)"
+
 for package in $@ #has to be next to eachother like no gap{1..10}
 do 
+dnf list installed $package
+if [ $? -ne 0 ];then
+dnf install $package
+else 
+    echo "$package is istalled already... $Y SKIPPING $N "
 echo $package
 done
