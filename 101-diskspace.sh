6f4ad4421ed5c08c446194fb51dfa4d95b7c15bc
ramana sanku <sankuramana456@gmail.com>
2025-12-12 13:55:31 +0530

    docker-roboshop

---
+++ 101-diskspace.sh
@@ -0,0 +1,12 @@
+DISK_USAGE=$(df -hT| grep -v Filesystem)
+DISK_THRESHOLD=2 #usaualy we check if disk usage is more than 75
+ 
+ while IFS=read -r space
+ do
+ MOUNT_ON=$(echo $space | awk '{ print $7}')
+ USAGE=$(echo $space | awk '{print $6}')|cut -d "%" -f1)
+ if [ $USAGE -gt $DISK_THRESHOLD ]; then
+    echo "HIGH DISK USAGE IS $MOUNT_ON : $USAGE"
+fi
+ done <<< $DISK_USAGE
+
