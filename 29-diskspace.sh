a712fe2a2a8f7b9e9fb838ed9172b0fe4275245b
ramana sanku <sankuramana456@gmail.com>
2025-12-12 12:31:04 +0530

    docker-roboshop

--- 29-diskspace.sh
+++ 29-diskspace.sh
@@ -6,7 +6,7 @@ while IFS= read -r line
 do 
 USAGE=$(echo $line | awk '{print $6}'| cut -d "%" -f1)
 PARTISION=$(echo $line |aws '{print $7}')
-if [ $USAGE -ge $DISK_THRESHOLD];then
+if [ $USAGE -ge $DISK_THRESHOLD ];then
     echo " Hig usage on $PARTISON: $USAGE"
 fi
 done <<< $DISK_USAGE
