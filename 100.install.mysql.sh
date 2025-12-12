c3d9ed40fb1c04a6d01cecc65cfb571a2ddbccba
ramana sanku <sankuramana456@gmail.com>
2025-12-12 06:36:54 +0530

    docker-roboshop

--- 100.install.mysql.sh
+++ 100.install.mysql.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 USERID=$(id -u)
+#chcek user is r
 if [ $USERID -ne 0 ]; then
     echo "please run this script with root user"
     exit 1 
 fi
 
-dnf install mysql -y
-if [ $? -ne 0 ]; then 
-    echo "mysql installation if failed "
-    exit 1
+dnf list installed mysql
+if [ $? -eq 0];then
+    echo "installed mysql already"
 else 
-    echo "mysql is installed succesfully"
+  dnf install mysql -y
 fi
