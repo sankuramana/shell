e579541495aabffbaa301ef188ea1d77d9b7e4a2
ramana sanku <sankuramana456@gmail.com>
2025-12-12 19:03:44 +0530

    docker-roboshop

--- 102.backup.sh
+++ 102.backup.sh
@@ -30,6 +30,10 @@ FILES=$(find $SOURCE_DIRECTORY -name "*.log" -type f -mtime +$DAYS)
 #donot forget to give sourec directory
 if [ ! -z "${FILES}" ];then  #double quotes
     echo "files found"
+    TIMESTAMP=$(date +%F-%M-%H-%S)
+    ZIP_FILENAME="$DESTINATION_DIRECTORY/app-logs-$TIMESTAMP.zip"
+    echo " zipfile name is $ZIP_FILENAME"
+
 else
     echo -e "no files to archive ....  $R SKIPPING $N"
 fi
