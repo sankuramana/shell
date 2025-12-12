695e00a1e4f7cdac763c57c444dde0cc2d90c667
ramana sanku <sankuramana456@gmail.com>
2025-12-12 12:32:43 +0530

    docker-roboshop

--- 29-diskspace.sh
+++ 29-diskspace.sh
@@ -5,7 +5,7 @@ DISK_THRESHOLD=2 #Usually we chcek avove 75% and mail if used above 75%
 while IFS= read -r line
 do 
 USAGE=$(echo $line | awk '{print $6}'| cut -d "%" -f1)
-PARTISION=$(echo $line |aws '{print $7}')
+PARTISION=$(echo $line |awk '{print $7}')
 if [ $USAGE -ge $DISK_THRESHOLD ];then
     echo " Hig usage on $PARTISON: $USAGE"
 fi
