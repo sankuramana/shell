15543d507f9957622f2344bbbfb412b098488e1c
ramana sanku <sankuramana456@gmail.com>
2025-12-12 18:54:48 +0530

    docker-roboshop

--- 102.backup.sh
+++ 102.backup.sh
@@ -26,7 +26,8 @@ if [ ! -d $DESTINATION_DIRECTORY ]; then
     echo " destination directory is $DESTINATION_DIRECTOR not exist"
 fi 
 #####find the .log file#############
-FILES=$(find -name "*.log" -type f -mtime +$DAYS)
+FILES=$(find $SOURCE_DIRECTORY -name "*.log" -type f -mtime +$DAYS) 
+#donot forget to give sourec directory
 if [ ! -z "${FILES}"];then  #double quotes
     echo "files found"
 else
