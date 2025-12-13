44017a9726681f1bc88346140f92cbabf87d2c9d
ramana sanku <sankuramana456@gmail.com>
2025-12-14 05:05:53 +0530

    docker-roboshop

--- 104.practice.sh
+++ 104.practice.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-# 1 to 100 print
-for i in {100..1}
-do 
-echo " print $i"
+# 1 to 100 print and 100 to 1
+
+# for i in {100..1}
+# do 
+# echo " print $i"
+# done
+###############################################################
+while i in {100..1}
+do
+echo "print $i"
 done
