b4dd9f2708f61b8b7852f3126fd8e9eeb461db1f
ramana sanku <sankuramana456@gmail.com>
2025-12-14 19:35:05 +0530

    docker-roboshop

--- 33-dircetoryexist.sh
+++ 33-dircetoryexist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIRECTORY=$1
 if [ -d $DIRECTORY ];then
-    echo "$DIRCETORY is exist:
+    echo "$DIRCETORY is exist"
 else 
-    echo " $DIRCETORY is not Exist "
+    echo "$DIRCETORY is not Exist "
 fi
