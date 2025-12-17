fed5857f23cf01753125df63226f17357588235d
ramana sanku <sankuramana456@gmail.com>
2025-12-17 17:15:41 +0530

    docker-roboshop

--- 34-files-in-dir.sh
+++ 34-files-in-dir.sh
@@ -1,10 +1,17 @@
-#!/bin/bash
-G="\e[32m"
-N="\e[0m"
-directory=/home/ec2-user/files-dircetory
-echo -e "$G Available Files in files-directory is: $N"
+# #!/bin/bash
+# G="\e[32m"
+# N="\e[0m"
+# directory=/home/ec2-user/files-dircetory
+# echo -e "$G Available Files in files-directory is: $N"
 
-for file in $directory/*.txt
+# for file in $directory/*.txt
+# do
+#   echo "$file"
+# done
+# #
+count=0
+for file in /home/ec2-user/files-dircetory/*
 do
-  echo "$file"
+  ((count++))
 done
+echo "Total files: $count"
