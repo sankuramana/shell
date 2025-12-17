7f3034d02c3a331a6fe9c0ae330bde42ec431d96
ramana sanku <sankuramana456@gmail.com>
2025-12-17 21:43:24 +0530

    docker-roboshop

--- 38-empty-files.sh
+++ 38-empty-files.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 directory=/home/ec2-user/files-dircetory
 count=0
-for emptyfiles in $directory
+for emptyfiles in $directory/*
 do
 if [ ! -s $emptyfiles ];then
 ((count++))
 fi
-
 done
+
 echo "$count"
