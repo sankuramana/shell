14e041ec188ef867809e6968e01401aababe1eba
ramana sanku <sankuramana456@gmail.com>
2025-12-18 10:27:08 +0530

    docker-roboshop

--- 500-practice.sh
+++ 500-practice.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 diskspace=$(df -hT | grep -v Filesystem)
 echo "disk space of server is:"
-echo "$diskspace"
+echo "$space"
 threshold=20
 
 while IFS= read -r space
 do
-useage=$($space |awk "print{$6}" |cut -d "%" -f1)
+useage=$(space |awk "print{$6}" |cut -d "%" -f1)
 
 done <<<$diskspace
