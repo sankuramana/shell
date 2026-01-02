5cad327b042e7fddfae73d270ec59eefd9ef77c3
ramana sanku <sankuramana456@gmail.com>
2026-01-02 10:40:04 +0530

    shell

--- 103-avilable-Mem.sh
+++ 103-avilable-Mem.sh
@@ -4,7 +4,7 @@ N="\e[0m"
 R="\e[31m"
 AVAILABLE_MEMORY=$(free -h |grep Mem | awk '{print $7}')
 FREE_SPACE_IN_MEMORY=$(free -h |grep Mem | awk '{print $4}')
-echo -e "${R} available memeory is $AVAILABLE_MEMORY ${N}"
-echo -e "${R} available free memory is $FREE_SPACE_IN_MEMORY ${N}"
+echo -e "${R} Available memeory is $AVAILABLE_MEMORY ${N}"
+echo -e "${R} Available Free memory is $FREE_SPACE_IN_MEMORY ${N}"
 #echo "✅ Available Memory: $AVAILABLE_MEMORY | 💡 Free Memory: $FREE_SPACE_IN_MEMORY"
 
