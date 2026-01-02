e3ebae890d08173940377cb134a26fc7599ef4d9
ramana sanku <sankuramana456@gmail.com>
2026-01-02 10:34:25 +0530

    shell

--- 103-avilable-Mem.sh
+++ 103-avilable-Mem.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 AVAILABLE_MEMORY=$(free -h |grep Mem | awk '{print $7}')
-FREE_SPACE_IN_MEMEORY=$(free -h |grep Mem | awk '{print $4}')
+FREE_SPACE_IN_MEMORY=$(free -h |grep Mem | awk '{print $4}')
 # echo "available memeory is $AVAILABLE_MEMORY"
 # echo "available free memory is $FREE_SPACE_IN_MEMEORY"
 echo "✅ Available Memory: $AVAILABLE_MEMORY | 💡 Free Memory: $FREE_SPACE_IN_MEMORY"
